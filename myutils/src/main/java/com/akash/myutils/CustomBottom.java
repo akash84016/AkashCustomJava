253beb04ffbd08a8dd159bb2dbe5d72bde05fcc3
Akash Lalpuriya <akashlalpuriya28@users.noreply.github.com>
2019-11-06 14:20:44 +0530

    add Camera

--- myutils/src/main/java/com/akash/myutils/CustomBottom.java
+++ myutils/src/main/java/com/akash/myutils/CustomBottom.java
@@ -1,11 +1,25 @@
 package com.akash.myutils;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+
 
 public class CustomBottom {
+
+    public static final int CAMERA_ACTION_PICK_REQUEST_CODE = 1003;
+    public static String currentPhotoPath = "";
 
     public static void changeTextColor(String activeColor, String deActiveColor, TextView tvActive, TextView tvDeactivie1, TextView tvDeactivie2, TextView tvDeactivie3, TextView tvDeactivie4, TextView tvDeactivie5, TextView tvDeactivie6) {
         if (tvActive != null) {
@@ -53,6 +67,41 @@ public class CustomBottom {
         if (imgDeactivie6 != null) {
             imgDeactivie6.setColorFilter(Color.parseColor(deActiveColor));
         }
+    }
+
+    public static void openCamera(AppCompatActivity appCompatActivity) {
+        Intent pictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File file;
+        try {
+            file = getImageFile(); // 1
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return;
+        }
+        Uri uri;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) // 2
+            uri = FileProvider.getUriForFile(appCompatActivity, BuildConfig.APPLICATION_ID.concat(".provider"), file);
+        else
+            uri = Uri.fromFile(file); // 3
+        pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri); // 4
+        appCompatActivity.startActivityForResult(pictureIntent, CAMERA_ACTION_PICK_REQUEST_CODE);
+    }
+
+    private static File getImageFile() throws IOException {
+        String imageFileName = "JPEG_" + System.currentTimeMillis() + "_";
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory
+                (Environment.DIRECTORY_DCIM), "Camera");
+        System.out.println(storageDir.getAbsolutePath());
+        if (storageDir.exists())
+            System.out.println("File exists");
+        else
+            System.out.println("File not exists");
+        File file = File.createTempFile(
+                imageFileName, ".jpg", storageDir
+        );
+        currentPhotoPath = "file:" + file.getAbsolutePath();
+        return file;
     }
 
 }
